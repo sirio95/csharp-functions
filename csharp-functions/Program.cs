0440f21e6cb62d9c998f62fb80d172ec0b9fecc7
sirio95 <sirio.iasevoli@gmail.com>
2023-04-06 15:51:40 +0200

    Add project files.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+
+namespace csharp_functions
+{
+    internal class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            int[] arrNumeri = { 2, 6, 7, 5, 3, 9 };
+
+            Console.WriteLine("Array Originale: ");
+            StampaArray(arrNumeri);
+            Console.WriteLine("Array Copiato al Quadrato: ");
+            StampaArray(ElevaArrayAlQuadrato(arrNumeri));
+            Console.WriteLine("Sempre array originale: ");
+            StampaArray(arrNumeri);
+
+            Console.WriteLine("Somma dei numeri nell'array originario: ");
+            Console.WriteLine(SommaElementiArray(arrNumeri));
+            Console.WriteLine("Somma dei numeri nell'array al quadrato: ");
+            Console.WriteLine(SommaElementiArray(ElevaArrayAlQuadrato(arrNumeri)));
+
+
+
+
+
+        }
+
+
+        public static void StampaArray(int[] array)
+        {
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}");
+                if (i < array.Length - 1)
+                    Console.Write(",");
+            }
+            Console.WriteLine("]");
+        }
+        public static int Quadrato(int numero)
+        {
+            return numero * numero;
+        }
+        public static int[] ElevaArrayAlQuadrato(int[] array)
+        {
+            int[] arrayCopy = (int[])array.Clone();
+            for(int i=0; i<arrayCopy.Length; i++)
+            {
+                arrayCopy[i] = Quadrato(arrayCopy[i]);
+            }
+            return arrayCopy;
+        }
+        public static int SommaElementiArray(int[] array)
+        {
+            int somma = array.Sum();
+            return somma;
+        } 
+
+
+    }
+}
